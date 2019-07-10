2d6e81041624dae1e34a439f37da1e3e5cff511a
Mark Huynh <mark.huynh@cdk.com>
2019-07-10 13:06:32 -0700

    Updates

---
+++ precache-manifest.dc7745a1569876b006cdc2bab6f696d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc13c47ab9e60100a3a5ad03565de449",
+    "url": "/kigaruweb/index.html"
+  },
+  {
+    "revision": "0138d7db321658b58469",
+    "url": "/kigaruweb/static/css/main.06862ba8.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/kigaruweb/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "0138d7db321658b58469",
+    "url": "/kigaruweb/static/js/main.a6cb7c92.chunk.js"
+  },
+  {
+    "revision": "ca08303686ec7d9e0b80",
+    "url": "/kigaruweb/static/js/runtime~main.47433b6f.js"
+  }
+]);
