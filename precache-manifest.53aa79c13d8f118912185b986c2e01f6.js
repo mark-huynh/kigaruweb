fb28e6a80363484192946546badbd3301b2dfd85
Mark <mark.huynh.oregon@gmail.com>
2019-07-28 16:07:30 -0700

    Updates

---
+++ precache-manifest.53aa79c13d8f118912185b986c2e01f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca08303686ec7d9e0b80",
+    "url": "/kigaruweb/static/js/runtime~main.47433b6f.js"
+  },
+  {
+    "revision": "c8d25556014c6cf411c0",
+    "url": "/kigaruweb/static/js/main.59728ec6.chunk.js"
+  },
+  {
+    "revision": "1d42122877439b403878",
+    "url": "/kigaruweb/static/js/2.fea9da9e.chunk.js"
+  },
+  {
+    "revision": "c8d25556014c6cf411c0",
+    "url": "/kigaruweb/static/css/main.dac2b91f.chunk.css"
+  },
+  {
+    "revision": "dd9829022533291041a4fd43b2c147e2",
+    "url": "/kigaruweb/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kigaruweb/precache-manifest.05d88bd94f82c69b2b2a33afc29aaa11.js"
+  "/kigaruweb/precache-manifest.53aa79c13d8f118912185b986c2e01f6.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kigaruweb/index.html"), {
+workbox.routing.registerNavigationRoute("/kigaruweb/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
