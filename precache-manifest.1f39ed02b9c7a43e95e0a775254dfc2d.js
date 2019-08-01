fe8ca96d3a1460e273837a7c45226d0e97e6e067
Mark Huynh <mark.huynh@cdk.com>
2019-08-01 12:39:48 -0700

    Updates

---
+++ precache-manifest.1f39ed02b9c7a43e95e0a775254dfc2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91e69747872c1ffa8418cb3777430fb7",
+    "url": "/kigaruweb/static/media/wall2.91e69747.jpg"
+  },
+  {
+    "revision": "ca08303686ec7d9e0b80",
+    "url": "/kigaruweb/static/js/runtime~main.47433b6f.js"
+  },
+  {
+    "revision": "f9705829fe2de170a0aa",
+    "url": "/kigaruweb/static/js/main.6eae93fb.chunk.js"
+  },
+  {
+    "revision": "b7e55efdb02d7931993b",
+    "url": "/kigaruweb/static/js/2.b4132ba1.chunk.js"
+  },
+  {
+    "revision": "f9705829fe2de170a0aa",
+    "url": "/kigaruweb/static/css/main.407d04d7.chunk.css"
+  },
+  {
+    "revision": "9e637015ca136f060cb03aeacba440af",
+    "url": "/kigaruweb/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kigaruweb/precache-manifest.bc10887cd7fb200a33ad314cf0894580.js"
+  "/kigaruweb/precache-manifest.1f39ed02b9c7a43e95e0a775254dfc2d.js"
 );
 
 workbox.clientsClaim();
